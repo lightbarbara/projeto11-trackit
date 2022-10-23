a62f2ff8c103d717d94b22857145c762a5458fcf
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-10-23 19:24:25 -0300

    login e cadastro com campos desabilitados

--- src/pages/Cadastro.js
+++ src/pages/Cadastro.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useState } from "react"
+import { ThreeDots } from "react-loader-spinner"
 import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import logo from '../assets/logo.png'
@@ -16,6 +17,8 @@ export default function Cadastro() {
         image: ''
     })
 
+    const [disabled, setDisabled] = useState(false)
+
     function handleForm(e) {
         setForm({
             ...form,
@@ -25,20 +28,33 @@ export default function Cadastro() {
 
     function cadastro(e) {
         e.preventDefault()
+        setDisabled(true)
         axios.post(`${BASE_URL}/auth/sign-up`, form)
             .then(res => navigate('/'))
-            .catch(err => console.log(err.response.data))
+            .catch(err => {
+                alert(err.response.data.message)
+                setDisabled(false)
+            })
     }
 
     return (
         <CadastroContainer>
             <img src={logo} alt='logo' />
             <Form onSubmit={cadastro}>
-                <input type='email' placeholder='email' value={form.email} name='email' required onChange={handleForm} />
-                <input type='password' placeholder='senha' value={form.password} name='password' required onChange={handleForm} />
-                <input type='text' placeholder='nome' value={form.name} name='name' required onChange={handleForm} />
-                <input type='url' placeholder='foto' value={form.image} name='image' required onChange={handleForm} />
-                <button type='submit'>Cadastrar</button>
+                <input disabled={disabled} type='email' placeholder='email' value={form.email} name='email' required onChange={handleForm} />
+                <input disabled={disabled} type='password' placeholder='senha' value={form.password} name='password' required onChange={handleForm} />
+                <input disabled={disabled} type='text' placeholder='nome' value={form.name} name='name' required onChange={handleForm} />
+                <input disabled={disabled} type='url' placeholder='foto' value={form.image} name='image' required onChange={handleForm} />
+                <button disabled={disabled} type='submit'>{disabled ? <ThreeDots
+                    height="10"
+                    width="375"
+                    radius="9"
+                    color="white"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClassName=""
+                    visible={true}
+                /> : 'Cadastrar'}</button>
             </Form>
             <Link to='/'><p>Já tem uma conta? Faça login!</p></Link>
         </CadastroContainer>
--- src/pages/TelaInicial.js
+++ src/pages/TelaInicial.js
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import logo from '../assets/logo.png'
 import { BASE_URL } from "../constants/urls"
+import { ThreeDots } from 'react-loader-spinner'
 
 export default function TelaInicial() {
 
@@ -14,6 +15,8 @@ export default function TelaInicial() {
         password: ''
     })
 
+    const [disabled, setDisabled] = useState(false)
+
     function handleForm(e) {
         setForm({
             ...form,
@@ -23,18 +26,31 @@ export default function TelaInicial() {
 
     function login(e) {
         e.preventDefault()
+        setDisabled(true)
         axios.post(`${BASE_URL}/auth/login`, form)
-            .then(res => navigate('/habitos'))
-            .catch(err => console.log(err.response.data))
+            .then(res => navigate('/hoje'))
+            .catch(err => {
+                alert(err.response.data.details)
+                setDisabled(false)
+            })
     }
 
     return (
         <TelaInicialContainer>
             <img src={logo} alt='logo' />
             <Form onSubmit={login}>
-                <input type='email' placeholder='email' value={form.email} name='email' required onChange={handleForm} />
-                <input type='password' placeholder='senha' value={form.password} name='password' required onChange={handleForm} />
-                <button type='submit'>Entrar</button>
+                <input disabled={disabled} type='email' placeholder='email' value={form.email} name='email' required onChange={handleForm} />
+                <input disabled={disabled} type='password' placeholder='senha' value={form.password} name='password' required onChange={handleForm} />
+                <button disabled={disabled} type='submit'>{disabled ? <ThreeDots
+                    height="10"
+                    width="375"
+                    radius="9"
+                    color="white"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClassName=""
+                    visible={true}
+                /> : 'Entrar'}</button>
             </Form>
             <Link to='/cadastro'><p>Não tem uma conta? Cadastre-se!</p></Link>
         </TelaInicialContainer>
